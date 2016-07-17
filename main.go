17e1cb43e54481d7af9552719b756827c6228c7b
Mark <mark.ernst.me@gmail.com>
2016-07-17 19:28:41 +0200

    Update main.go

--- main.go
+++ main.go
@@ -11,10 +11,10 @@ import (
 
 	"github.com/yvasiyarov/gorelic"
 
-	"github.com/sammy007/open-ethereum-pool/api"
-	"github.com/sammy007/open-ethereum-pool/payouts"
-	"github.com/sammy007/open-ethereum-pool/proxy"
-	"github.com/sammy007/open-ethereum-pool/storage"
+	"github.com/LeChuckDE/open-expanse-pool/api"
+	"github.com/LeChuckDE/open-expanse-pool/payouts"
+	"github.com/LeChuckDE/open-expanse-pool/proxy"
+	"github.com/LeChuckDE/open-expanse-pool/storage"
 )
 
 var cfg proxy.Config
